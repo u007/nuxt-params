8f13968949bf9ebc8b125834e6e5aa08081661d1
james <james@mercstudio.com>
2022-11-15 17:29:15 +0800

    initial

---
+++ pages/hi/[name]/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Hi {{ $route.params.name }}
+  </div>
+  <nuxt-link to="/">
+    back
+  </nuxt-link>
+</template>
+
+<script lang="ts" setup>
+
+const $route = useRoute()
+
+definePageMeta({
+  layout: "default",
+});
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  AAA
+  <nuxt-link to="/hi/abcme">
+    abc
+  </nuxt-link>
+  <br />
+
+  <nuxt-link to="/hi2/abcme">
+    abc 2
+  </nuxt-link>
+</template>
+
+<script lang="ts" setup>
+
+definePageMeta({
+  layout: "default",
+});
+</script>
